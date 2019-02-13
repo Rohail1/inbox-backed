68fa8bafdbd8600de3f9863203729a0070c6d568
Rohail Najam <rohail_najam@hotmail.com>
2019-02-13 21:48:39 +0500

    Added basic user admin.

--- inbox/users/admin.py
+++ inbox/users/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from custom_user.admin import EmailUserAdmin
+from users.models import User
 # Register your models here.
+
+
+@admin.register(User)
+class CustomUserAdmin(EmailUserAdmin):
+    model = User
+    list_display = ('email', 'date_of_birth',)
